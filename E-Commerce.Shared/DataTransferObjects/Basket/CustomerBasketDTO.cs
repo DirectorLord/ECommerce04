42d3ee8cc94184f8d6ed54f3fb3f6e264a60ee96
Director Lord <thelord161203@gmail.com>
2025-10-21 21:37:31 +0300

    basket module finished

---
+++ E-Commerce.Shared/DataTransferObjects/Basket/CustomerBasketDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace E_Commerce.Shared.DataTransferObjects.Basket;
+
+public class CustomerBasketDTO
+{
+    public string Id { get; set; }
+    public ICollection<BasketItemDTO> MyProperty { get; set; }
+}
+public class BasketItemDTO
+{
+#nullable disable
+    public int Id { get; set; }
+    public string Name { get; init; }
+    public string Description { get; init; }
+    public string PictureUrl { get; init; }
+    public decimal Price { get; init; }
+    public int Quantity { get; set; }
+}
