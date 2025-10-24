1c0e00b6a50407e46dae665777e2782038c1f602
Director Lord <thelord161203@gmail.com>
2025-10-24 13:46:55 +0300

    finished redis no sql

--- E-Commerce.Shared/DataTransferObjects/Basket/CustomerBasketDTO.cs
+++ E-Commerce.Shared/DataTransferObjects/Basket/CustomerBasketDTO.cs
@@ -3,15 +3,5 @@
 public class CustomerBasketDTO
 {
     public string Id { get; set; }
-    public ICollection<BasketItemDTO> MyProperty { get; set; }
-}
-public class BasketItemDTO
-{
-#nullable disable
-    public int Id { get; set; }
-    public string Name { get; init; }
-    public string Description { get; init; }
-    public string PictureUrl { get; init; }
-    public decimal Price { get; init; }
-    public int Quantity { get; set; }
+    public ICollection<BasketItemDTO> Items { get; set; }
 }
