66fe1568115d875078b5bb917ecf71ac55390cd7
Director Lord <thelord161203@gmail.com>
2025-10-24 15:02:53 +0300

    finished attributes

--- ECommerce.Web/Controllers/ProductsController.cs
+++ ECommerce.Web/Controllers/ProductsController.cs
@@ -1,48 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using E_Commerce.Presentation.API.Attributes;
 
 namespace ECommerce.Web.Controllers;
 [ApiController, Route("api/[Controller]")]
 public class ProductsController : ControllerBase
 {
-    //#region GetID
-    //[HttpGet("Products/{id}")]
-    //public ActionResult Get(int id)
-    //{
-    //    return Ok(new Product { Id = id });
-    //} 
-    //#endregion
+    #region GetID
+    [RedisCash]
+    [HttpGet("Products/{id}")]
+    public ActionResult Get(int id)
+    {
+        return Ok(new Product { Id = id });
+    }
+    #endregion
 
-    //#region GetAll
-    //[HttpGet("/Products")]
-    //public ActionResult GetAll()
-    //{
-    //    return Ok(new Product { });
-    //}
-    //#endregion
+    #region GetAll
+    [HttpGet("/Products")]
+    public ActionResult GetAll()
+    {
+        return Ok(new Product { });
+    }
+    #endregion
 
-    //#region Post
-    //[HttpPost]
-    //public ActionResult Create(Product product)
-    //{
-    //    return Created("Test", product);
-    //}
-    //#endregion
+    #region Post
+    [HttpPost]
+    public ActionResult Create(Product product)
+    {
+        return Created("Test", product);
+    }
+    #endregion
 
-    //#region Edit
-    //[HttpPut]
-    //public ActionResult Update(Product product)
-    //{
-    //    return Ok(product);
-    //}
-    //#endregion
+    #region Edit
+    [HttpPut]
+    public ActionResult Update(Product product)
+    {
+        return Ok(product);
+    }
+    #endregion
 
-    //#region Delete
-    //[HttpDelete]
-    //public ActionResult Delete(int id)
-    //{
-    //    return NoContent();
-    //} 
-    //#endregion
+    #region Delete
+    [HttpDelete]
+    public ActionResult Delete(int id)
+    {
+        return NoContent();
+    }
+    #endregion
 }
 public class Product
 {
---
+++ Infrastructure/E-Commerce.Presentation.API/Attributes/RedisCashAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace E_Commerce.Presentation.API.Attributes;
+
+public class RedisCashAttribute : ActionFilterAttribute
+{
+    public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        next.Invoke();
+        return base.OnActionExecutionAsync(context, next);
+    }
+}
